f35f5a80916a8cef3c3de85fbff5bfe7750b4395
amamanku26 <amankumaranku@gmail.com>
2020-12-06 16:51:48 +0530

    Fetch-NewsData/added-TopNews/banner/NewsApi Components

---
+++ front-end/src/Components/NewsCompents/NewsApi.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react"
+import { useDispatch } from 'react-redux'
+import {getNewsData} from "../../Redux/NewsRedux/ActionCreator"
+import Banner from "./Banner"
+import TopNews from "./TopNews"
+
+function NewsApi() {
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    return  dispatch(getNewsData())
+  });
+
+  return (
+    <div className="App">
+      <Banner />
+      <TopNews />
+    </div>
+  );
+}
+
+export default NewsApi;
+// 1c27e766c09a4c0aad35869cdf3bc0d3
+// https://newsapi.org/v2/top-headlines?country=us&apiKey=1c27e766c09a4c0aad35869cdf3bc0d3
